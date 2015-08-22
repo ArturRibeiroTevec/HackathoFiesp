d3c31f5e414fbe4f7d2200d09b934588f9556423
Irineu Antunes <irineuantunes@Irineus-MacBook-Pro.local>
2015-08-22 16:44:39 -0300

    Adicionado imagem de skyline

--- controller/ViewController.js
+++ controller/ViewController.js
@@ -1,7 +1,7 @@
 var http = require(__dirname + "/../lib/http");
 
 http.get("/", http.noCache, function(req,res){
-	
+	res.json({status : "Hello"});
 });
 
 http.get("/auth/facebook/failed", http.noCache, function(req,res){
